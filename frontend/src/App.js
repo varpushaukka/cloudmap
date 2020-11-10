646b4656f8c1e7318d1a9d34f0c96e88929e26e7
Henna Kalliokoski <henna.kalliokoski@siili.com>
2020-11-10 19:17:02 +0200

    add cloud markers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { TileLayer, MapContainer, Marker, Popup, Circle } from "react-leaflet";
+import L from "leaflet";
+import { usePosition } from "use-position";
+
+import styled from "styled-components";
+
+import "leaflet/dist/leaflet.css";
 
 import {
   useQuery,
@@ -9,49 +15,93 @@ import {
   ApolloProvider,
 } from "@apollo/client";
 
+const cloudMarker = (color) =>
+  L.icon({
+    iconSize: [25, 41],
+    iconAnchor: [10, 41],
+    popupAnchor: [2, -40],
+    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  });
+
+const Map = styled(MapContainer)`
+  width: 100vw;
+  height: 100vh;
+`;
+
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
 });
 
 const CLOUDS = gql`
-  query GetClouds {clouds(name: "aws") {name geoRegion}}
+  query GetClouds {
+    clouds(name: "") {
+      name
+      geoRegion
+      geoLatitude
+      geoLongitude
+      cloudProvider
+    }
+  }
 `;
 
-function Clouds() {
+function Clouds(props) {
+  const colors = {
+    google: "green",
+    aws: "orange",
+    azure: "blue",
+    do: "grey",
+    upcloud: "yellow",
+  };
+  return props.data.clouds.map(
+    ({ name, geoRegion, geoLatitude, geoLongitude, cloudProvider }) => (
+      <Marker
+        position={[geoLatitude, geoLongitude]}
+        icon={cloudMarker(colors[cloudProvider])}
+      >
+        <Popup>
+          {name} <br /> {geoRegion}
+        </Popup>
+      </Marker>
+    )
+  );
+}
+
+function Markers() {
   const { loading, error, data } = useQuery(CLOUDS);
+  const { latitude, longitude } = usePosition();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  return data.clouds.map(({ name, geoRegion }) => (
-    <div key={name}>
-      <p>
-        {name}: {geoRegion}
-      </p>
-    </div>
-  ));
+  return (
+    <>
+      <Clouds data={data} />
+      <Circle
+        center={{ lat: latitude, lng: longitude }}
+        color="red"
+        fillColor="red"
+        radius={5000}
+      >
+        <Popup>Your Location</Popup>
+      </Circle>
+    </>
+  );
 }
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Clouds />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Map center={[62, 24]} zoom={7}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Markers />
+        </Map>
       </div>
     </ApolloProvider>
   );
